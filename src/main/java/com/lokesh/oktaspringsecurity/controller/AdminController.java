d4676b85cba6d2cf76e1651dc444e9e124f382fb
Lokesh Singh <lsingh@nisum.com>
2023-04-14 12:53:01 +0530

    get user details from okta spring sdk, seted sdk token

---
+++ src/main/java/com/lokesh/oktaspringsecurity/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.lokesh.oktaspringsecurity.controller;
+
+import com.okta.sdk.client.Client;
+import com.okta.sdk.resource.user.UserList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+    @Autowired
+    public Client client;
+    @GetMapping("/users")
+    public UserList getUsers() {
+        return client.listUsers();
+    }
+    @GetMapping("/user")
+    public UserList searchUserByEmail(@RequestParam String query) {
+        return client.listUsers(query, null, null, null, null);
+    }
+}
