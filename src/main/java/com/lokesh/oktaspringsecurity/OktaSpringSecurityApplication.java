058b68ad272c73664de5db0cc28fb741090ce5e9
Lokesh Singh <lsingh@nisum.com>
2023-04-14 11:14:18 +0530

    initial commit

---
+++ src/main/java/com/lokesh/oktaspringsecurity/OktaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lokesh.oktaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokesh/oktaspringsecurity/OktaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokesh.oktaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
