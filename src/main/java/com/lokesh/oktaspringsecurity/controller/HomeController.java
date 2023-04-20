d22c30c2da9cd7d5d830f928149beef049b780cc
Lokesh Singh <lsingh@nisum.com>
2023-04-20 16:25:40 +0530

    redirecting to angular

--- src/main/java/com/lokesh/oktaspringsecurity/controller/HomeController.java
+++ src/main/java/com/lokesh/oktaspringsecurity/controller/HomeController.java
@@ -4,11 +4,23 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
+//    @GetMapping("/")
+//    public String home(@AuthenticationPrincipal OidcUser user) {
+//        return "Welcome, "+ user.getFullName() + "!";
+//    }
+
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal OidcUser user) {
-        return "Welcome, "+ user.getFullName() + "!";
+    public ModelAndView homeToAngular(@AuthenticationPrincipal OidcUser user) {
+        String fullName = user.getFullName();
+        String accessToken = user.getIdToken().getTokenValue();
+        ModelAndView modelAndView = new ModelAndView("redirect:http://localhost:4200/Home");
+        modelAndView.addObject("fullName", fullName);
+        modelAndView.addObject("accessToken", accessToken);
+        return modelAndView;
     }
+
 }
